4090fe029119c14fcaabdf6b7d357d7169521e21
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-11-27 03:59:48 +0700

    chore: konfigurasi awal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./application/views/**/*.{php,html}", "./public/**/*.{php,html}"],
+	content: ["./application/**/*.{php,html}", "./public/**/*.{php,html}"],
 	theme: {
 		extend: {
 			container: {
 				center: true,
+				"2xl": "1320px",
+			},
+			fontFamily: {
+				"open-sans": ["Open-Sans", "sans-serif"],
+				rubik: ["Rubik", "sans-serif"],
+			},
+			colors: {
+				"light-gray": "#E7E7E3",
+				"off-white": "#FAFAFA",
+				"dark-charcoal": "#232321",
+				"royal-blue": "#4A69E2",
+				"golden-orange": "#FFA52F",
+			},
+			screens: {
+				"2xl": "1320px",
 			},
 		},
 	},
