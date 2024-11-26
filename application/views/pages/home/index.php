68d6b01defc2d60757b0d1dfe614f13a1dae2005
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-11-27 04:01:38 +0700

    add: menambahkan hero section

--- application/views/pages/home/index.php
+++ application/views/pages/home/index.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Home</h1>
-</body>
-
-</html>
+<!-- hero section -->
+<section class="mt-[24px] flex flex-col gap-[24px] items-center justify-center">
+    <h1 class="font-rubik font-bold text-[48px] uppercase lg:text-[180px]">Easy to
+        <span class="text-royal-blue">Move</span>
+    </h1>
+    <img src="<?= base_url('public/images/slider-image-1.png'); ?>" class="md:container block w-full rounded-[24px] max-h-[750px] bg-center bg-cover lg:rounded-[64px]" alt="slider-image-1" loading="lazy">
+</section>
+<!-- new products section -->
+<section></section>
