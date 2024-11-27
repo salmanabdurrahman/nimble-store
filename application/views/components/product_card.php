af52f2fd3598f06bb0e0d4a6eeb9c3c3367b7fe2
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-11-27 23:46:28 +0700

    fix: memperbaiki product card

--- application/views/components/product_card.php
+++ application/views/components/product_card.php
@@ -1,13 +1,11 @@
-<!-- card -->
-<div class="flex flex-col gap-2 w-full max-w-[171px] lg:max-w-[318px]">
-    <div class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px] lg:max-w-[318px]">
+<div class="flex flex-col gap-2 w-full">
+    <div class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px]">
         <img src="<?= base_url('public/uploads/' . $product['image_url']); ?>" alt="<?= $product['name']; ?>"
-            class="block w-full h-full max-w-[155px] max-h-[164px] rounded-xl lg:rounded-3xl lg:max-w-[302px] lg:max-h-[334px]"
-            loading="lazy">
+            class="block w-full h-full max-h-[164px] rounded-xl lg:rounded-3xl lg:max-h-[334px]" loading="lazy">
     </div>
     <h3 class="font-rubik font-semibold text-base text-dark-charcoal lg:text-2xl uppercase"><?= $product['name']; ?>
     </h3>
     <button type="button"
-        class="bg-dark-charcoal rounded-lg w-full max-w-[171px] font-rubik font-medium text-xs text-white tracking-wider py-[13px] px-5 lg:text-sm lg:py-[15.5px] lg:px-[82px] lg:max-w-[318px] uppercase">View
-        Product - <span class="text-golden-orange">$<?= $product['price']; ?></span></button>
+        class="bg-dark-charcoal rounded-lg w-full font-rubik font-medium text-xs text-white tracking-wider py-[13px] lg:text-sm lg:py-[15.5px] uppercase">View
+        Product - <span class="text-golden-orange inline-block">$<?= $product['price']; ?></span></button>
 </div>
--- application/views/pages/home/index.php
+++ application/views/pages/home/index.php
@@ -44,56 +44,48 @@
     </div>
     <div class="grid grid-cols-2 gap-y-6 gap-x-4 items-center justify-center w-full lg:gap-4 lg:grid-cols-4">
         <!-- card -->
-        <div class="flex flex-col gap-2 w-full max-w-[171px] lg:max-w-[318px]">
-            <div
-                class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px] lg:max-w-[318px]">
+        <div class="flex flex-col gap-2 w-full">
+            <div class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px]">
                 <img src="<?= base_url('public/images/dummy/dummy-product.png'); ?>" alt="dummy-product"
-                    class="block w-full h-full max-w-[155px] max-h-[164px] rounded-xl lg:rounded-3xl lg:max-w-[302px] lg:max-h-[334px]"
-                    loading="lazy">
+                    class="block w-full h-full max-h-[164px] rounded-xl lg:rounded-3xl lg:max-h-[334px]" loading="lazy">
             </div>
             <h3 class="font-rubik font-semibold text-base text-dark-charcoal lg:text-2xl uppercase">ADIDAS 4DFWD X
                 PARLEY RUNNING SHOES</h3>
             <button type="button"
-                class="bg-dark-charcoal rounded-lg w-full max-w-[171px] font-rubik font-medium text-xs text-white tracking-wider py-[13px] lg:text-sm lg:py-[15.5px] lg:max-w-[318px] uppercase">View
+                class="bg-dark-charcoal rounded-lg w-full font-rubik font-medium text-xs text-white tracking-wider py-[13px] lg:text-sm lg:py-[15.5px] uppercase">View
                 Product - <span class="text-golden-orange inline-block">$125</span></button>
         </div>
-        <div class="flex flex-col gap-2 w-full max-w-[171px] lg:max-w-[318px]">
-            <div
-                class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px] lg:max-w-[318px]">
+        <div class="flex flex-col gap-2 w-full">
+            <div class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px]">
                 <img src="<?= base_url('public/images/dummy/dummy-product.png'); ?>" alt="dummy-product"
-                    class="block w-full h-full max-w-[155px] max-h-[164px] rounded-xl lg:rounded-3xl lg:max-w-[302px] lg:max-h-[334px]"
-                    loading="lazy">
+                    class="block w-full h-full max-h-[164px] rounded-xl lg:rounded-3xl lg:max-h-[334px]" loading="lazy">
             </div>
             <h3 class="font-rubik font-semibold text-base text-dark-charcoal lg:text-2xl uppercase">ADIDAS 4DFWD X
                 PARLEY RUNNING SHOES</h3>
             <button type="button"
-                class="bg-dark-charcoal rounded-lg w-full max-w-[171px] font-rubik font-medium text-xs text-white tracking-wider py-[13px] lg:text-sm lg:py-[15.5px] lg:max-w-[318px] uppercase">View
+                class="bg-dark-charcoal rounded-lg w-full font-rubik font-medium text-xs text-white tracking-wider py-[13px] lg:text-sm lg:py-[15.5px] uppercase">View
                 Product - <span class="text-golden-orange inline-block">$125</span></button>
         </div>
-        <div class="flex flex-col gap-2 w-full max-w-[171px] lg:max-w-[318px]">
-            <div
-                class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px] lg:max-w-[318px]">
+        <div class="flex flex-col gap-2 w-full">
+            <div class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px]">
                 <img src="<?= base_url('public/images/dummy/dummy-product.png'); ?>" alt="dummy-product"
-                    class="block w-full h-full max-w-[155px] max-h-[164px] rounded-xl lg:rounded-3xl lg:max-w-[302px] lg:max-h-[334px]"
-                    loading="lazy">
+                    class="block w-full h-full max-h-[164px] rounded-xl lg:rounded-3xl lg:max-h-[334px]" loading="lazy">
             </div>
             <h3 class="font-rubik font-semibold text-base text-dark-charcoal lg:text-2xl uppercase">ADIDAS 4DFWD X
                 PARLEY RUNNING SHOES</h3>
             <button type="button"
-                class="bg-dark-charcoal rounded-lg w-full max-w-[171px] font-rubik font-medium text-xs text-white tracking-wider py-[13px] lg:text-sm lg:py-[15.5px] lg:max-w-[318px] uppercase">View
+                class="bg-dark-charcoal rounded-lg w-full font-rubik font-medium text-xs text-white tracking-wider py-[13px] lg:text-sm lg:py-[15.5px] uppercase">View
                 Product - <span class="text-golden-orange inline-block">$125</span></button>
         </div>
-        <div class="flex flex-col gap-2 w-full max-w-[171px] lg:max-w-[318px]">
-            <div
-                class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px] lg:max-w-[318px]">
+        <div class="flex flex-col gap-2 w-full">
+            <div class="mb-2 w-full bg-off-white rounded-2xl h-[171px] p-2 lg:rounded-[28px] lg:h-[350px]">
                 <img src="<?= base_url('public/images/dummy/dummy-product.png'); ?>" alt="dummy-product"
-                    class="block w-full h-full max-w-[155px] max-h-[164px] rounded-xl lg:rounded-3xl lg:max-w-[302px] lg:max-h-[334px]"
-                    loading="lazy">
+                    class="block w-full h-full max-h-[164px] rounded-xl lg:rounded-3xl lg:max-h-[334px]" loading="lazy">
             </div>
             <h3 class="font-rubik font-semibold text-base text-dark-charcoal lg:text-2xl uppercase">ADIDAS 4DFWD X
                 PARLEY RUNNING SHOES</h3>
             <button type="button"
-                class="bg-dark-charcoal rounded-lg w-full max-w-[171px] font-rubik font-medium text-xs text-white tracking-wider py-[13px] lg:text-sm lg:py-[15.5px] lg:max-w-[318px] uppercase">View
+                class="bg-dark-charcoal rounded-lg w-full font-rubik font-medium text-xs text-white tracking-wider py-[13px] lg:text-sm lg:py-[15.5px] uppercase">View
                 Product - <span class="text-golden-orange inline-block">$125</span></button>
         </div>
     </div>
