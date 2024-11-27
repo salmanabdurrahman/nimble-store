a9839ba1dc2c5459a0a7071cd865fb436f2a8c17
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-11-27 22:14:26 +0700

    add: menambahkan brands section

--- application/views/pages/home/index.php
+++ application/views/pages/home/index.php
@@ -1,11 +1,32 @@
 <!-- hero section -->
-<section class="mt-[32px] flex flex-col gap-[24px] items-center justify-center lg:mt-[60px]">
+<section class="mt-[32px] flex flex-col gap-[24px] items-center justify-center lg:mt-[60px] container">
     <!-- <h1 class="font-rubik font-bold text-[48px] uppercase lg:text-[180px]">Easy to
         <span class="text-royal-blue">Move</span>
     </h1> -->
     <img src="<?= base_url('public/images/slider/slider-image-1.png'); ?>"
-        class="container block w-full rounded-[24px] max-h-[750px] bg-center bg-cover lg:rounded-[64px]"
-        alt="slider-image-1" loading="lazy">
+        class="block w-full rounded-[24px] max-h-[750px] bg-center bg-cover lg:rounded-[64px]" alt="slider-image-1"
+        loading="lazy">
 </section>
-<!-- new products section -->
-<section></section>
+<!-- brands section -->
+<section class="mt-[32px] flex flex-col items-center justify-center gap-[30px] container lg:mt-[100px] lg:gap-[50px]">
+    <h3 class="font-rubik text-[30px] font-semibold leading-[95%] text-dark-charcoal lg:text-[74px]">OUR BRANDS</h3>
+    <div
+        class="flex flex-col items-center justify-center gap-y-[27px] border-solid border-t-[3px] border-b-[3px] border-y-[rgba(35,35,33,0.2)] py-[15px] w-full lg:flex-row lg:gap-y-[0px] lg:gap-x-[138px]">
+        <div class="flex gap-x-[64px] items-center justify-center lg:gap-x-[138px]">
+            <img src="<?= base_url('public/images/brands/adidas.png'); ?>" class="block w-full" alt="adidas"
+                loading="lazy">
+            <img src="<?= base_url('public/images/brands/converse.png'); ?>" class="block w-full" alt="converse"
+                loading="lazy">
+        </div>
+        <div class="flex gap-x-[64px] items-center justify-center lg:gap-x-[138px]">
+            <img src="<?= base_url('public/images/brands/puma.png'); ?>" class="block w-full" alt="puma" loading="lazy">
+            <img src="<?= base_url('public/images/brands/reebok.png'); ?>" class="block w-full" alt="reebok"
+                loading="lazy">
+        </div>
+        <div class="flex gap-x-[64px] items-center justify-center lg:gap-x-[138px]">
+            <img src="<?= base_url('public/images/brands/new-balance.png'); ?>" class="block w-full" alt="new-balance"
+                loading="lazy">
+            <img src="<?= base_url('public/images/brands/nike.png'); ?>" class="block w-full" alt="nike" loading="lazy">
+        </div>
+    </div>
+</section>
