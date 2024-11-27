526b0100cf5ce47590f5d08e470f1fdc02c1a0c3
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-11-27 16:00:14 +0700

    fix: penyesuaian hero section

--- application/views/pages/home/index.php
+++ application/views/pages/home/index.php
@@ -1,9 +1,11 @@
 <!-- hero section -->
-<section class="mt-[24px] flex flex-col gap-[24px] items-center justify-center">
-    <h1 class="font-rubik font-bold text-[48px] uppercase lg:text-[180px]">Easy to
+<section class="mt-[32px] flex flex-col gap-[24px] items-center justify-center lg:mt-[60px]">
+    <!-- <h1 class="font-rubik font-bold text-[48px] uppercase lg:text-[180px]">Easy to
         <span class="text-royal-blue">Move</span>
-    </h1>
-    <img src="<?= base_url('public/images/slider-image-1.png'); ?>" class="md:container block w-full rounded-[24px] max-h-[750px] bg-center bg-cover lg:rounded-[64px]" alt="slider-image-1" loading="lazy">
+    </h1> -->
+    <img src="<?= base_url('public/images/slider-image-1.png'); ?>"
+        class="container block w-full rounded-[24px] max-h-[750px] bg-center bg-cover lg:rounded-[64px]"
+        alt="slider-image-1" loading="lazy">
 </section>
 <!-- new products section -->
 <section></section>
