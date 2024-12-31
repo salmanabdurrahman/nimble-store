c253115871dfcc02de5f9e8c95ae45d4cefed5ed
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-12-31 13:15:37 +0700

    add: menambahkan halaman about

--- application/views/pages/about/index.php
+++ application/views/pages/about/index.php
@@ -1,14 +1,59 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- ======= ABOUT SECTION START ======= -->
+<section
+    class="mt-[60px] bg-off-white container rounded-3xl py-[130px] px-[260px] flex flex-col gap-[15px] items-center justify-center">
+    <h1 class="font-rubik font-semibold text-dark-charcoal text-[50px]">NIMBLE <span
+            class="text-royal-blue">STORE</span></h1>
+    <p class="font-open-sans font-normal text-dark-charcoal text-[20px] text-center">Lorem Ipsum is simply dummy text of
+        the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an </p>
+</section>
+<!-- ======= ABOUT SECTION END ======= -->
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<!-- ======= TESTIMONIAL SECTION START ======= -->
+<section class="w-full grid grid-cols-3 gap-[60px] items-stretch justify-center mt-[42px] container">
+    <div class="bg-off-white w-full rounded-3xl flex items-center justify-center py-[150px] px-[40px] gap-5">
+        <p class="text-royal-blue font-rubik font-semibold text-[50px]">20+</p>
+        <p class="text-dark-charcoal font-rubik font-semibold text-xl">Tersedia <span class="text-royal-blue">Berbagai
+                Jenis</span> Sepatu</p>
+    </div>
+    <div class="bg-off-white w-full rounded-3xl flex items-center justify-center py-[150px] px-[40px] gap-5">
+        <p class="text-royal-blue font-rubik font-semibold text-[50px]">150+</p>
+        <p class="text-dark-charcoal font-rubik font-semibold text-xl">Kami telah <span class="text-royal-blue">menjual
+                ratusan </span>produk sepatu</p>
+    </div>
+    <div class="bg-off-white w-full rounded-3xl flex items-center justify-center py-[150px] px-[40px] gap-5">
+        <p class="text-royal-blue font-rubik font-semibold text-[50px]">100+</p>
+        <p class="text-dark-charcoal font-rubik font-semibold text-xl">Kami memiliki <span
+                class="text-royal-blue">ratusan pelanggan</span> yang terdaftar
+        </p>
+    </div>
+</section>
+<!-- ======= TESTIMONIAL SECTION END ======= -->
 
-<body>
-    <h1>About</h1>
-</body>
-
-</html>
+<!-- ======= TEAM SECTION START ======= -->
+<section class="container mt-[100px] flex flex-col items-center justify-center gap-[60px] w-full">
+    <h2 class="font-rubik font-semibold text-[74px] text-dark-charcoal">OUR TEAMS</h2>
+    <div class="flex flex-wrap w-full max-w-[1130px] items-center justify-center gap-x-[35px] gap-y-[17px]">
+        <div class="size-[350px] rounded-[28px] p-2 bg-off-white overflow-hidden">
+            <img src="<?= base_url('public/images/about/aziz.jpg'); ?>" alt="profile"
+                class="block w-full h-full rounded-[28px] object-cover" loading="lazy">
+        </div>
+        <div class="size-[350px] rounded-[28px] p-2 bg-off-white overflow-hidden">
+            <img src="<?= base_url('public/images/about/wisnu.jpg'); ?>" alt="profile"
+                class="block w-full h-full rounded-[28px] object-cover" loading="lazy">
+        </div>
+        <div class="size-[350px] rounded-[28px] p-2 bg-off-white overflow-hidden">
+            <img src="<?= base_url('public/images/about/contoh.JPG'); ?>" alt="profile"
+                class="block w-full h-full rounded-[28px] object-cover" loading="lazy">
+        </div>
+        <div class="size-[350px] rounded-[28px] p-2 bg-off-white overflow-hidden">
+            <img src="<?= base_url('public/images/about/salman.jpg'); ?>" alt="profile"
+                class="block w-full h-full rounded-[28px] object-cover" loading="lazy">
+        </div>
+        <div class="size-[350px] rounded-[28px] p-2 bg-off-white overflow-hidden">
+            <img src="<?= base_url('public/images/about/contoh.JPG'); ?>" alt="profile"
+                class="block w-full h-full rounded-[28px] object-cover" loading="lazy">
+        </div>
+    </div>
+</section>
+<!-- ======= TEAM SECTION END ======= -->
